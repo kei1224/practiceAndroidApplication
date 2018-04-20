99580ced0015da89c86fd8ce81d5eeb7bbb351ef
Yamashita Satoshi <satoshi.yamashita1224@gmail.com>
2018-04-20 14:20:58 +0900

    Kotlinスタートブックの全項目を完了

--- QiitaClient/app/src/main/java/jp/gr/java_conf/yamashita/qiitaclient/MainActivity.kt
+++ QiitaClient/app/src/main/java/jp/gr/java_conf/yamashita/qiitaclient/MainActivity.kt
@@ -12,24 +12,38 @@ import jp.gr.java_conf.yamashita.qiitaclient.client.ArticleClient
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 import javax.inject.Inject
+import kotterknife.bindView
+import org.jetbrains.anko.setContentView
 
 class MainActivity : RxAppCompatActivity() {
     @Inject
     lateinit var articleClient: ArticleClient
 
+    /*
+    val listView by bindView<ListView>(R.id.list_view)
+    val progressBar by bindView<ProgressBar>(R.id.progress_bar)
+    val searchButton by bindView<Button>(R.id.search_button)
+    val queryEditText by bindView<EditText>(R.id.query_edit_text)*/
+    private val ui: MainActivityUI by lazy {
+        MainActivityUI().apply { setContentView(this@MainActivity) }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (application as QiitaClientApp).component.inject(this)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
 
+        /*
         val listView: ListView = findViewById(R.id.list_view)
         val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
         val queryEditText = findViewById<EditText>(R.id.query_edit_text)
-        val searchButton = findViewById<Button>(R.id.search_button)
+        val searchButton = findViewById<Button>(R.id.search_button)*/
+
 
         val listAdapter = ArticleListAdapter(applicationContext)
-        listView.adapter = listAdapter
-        listView.setOnItemClickListener{ adapterView, view, position, id ->
+        // listView.adapter = listAdapter
+        ui.listView.adapter = listAdapter
+        ui.listView.setOnItemClickListener{ adapterView, view, position, id ->
             val intent = ArticleActivity.intent(this, listAdapter.articles[position])
             startActivity(intent)
         }
@@ -45,18 +59,18 @@ class MainActivity : RxAppCompatActivity() {
                 .build()
         val articleClient = retrofit.create(ArticleClient::class.java)*/
 
-        searchButton.setOnClickListener {
-            progressBar.visibility = View.VISIBLE
+        ui.searchButton.setOnClickListener {
+            ui.progressBar.visibility = View.VISIBLE
 
-            articleClient.search(queryEditText.text.toString())
+            articleClient.search(ui.queryEditText.text.toString())
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .doAfterTerminate{
-                        progressBar.visibility = View.GONE
+                        ui.progressBar.visibility = View.GONE
                     }
                     .bindToLifecycle(this)
                     .subscribe({
-                        queryEditText.text.clear()
+                        ui.queryEditText.text.clear()
                         listAdapter.articles = it
                         listAdapter.notifyDataSetChanged()
                     }, {
